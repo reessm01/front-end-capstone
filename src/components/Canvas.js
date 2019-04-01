ce70227a6461a7ab99c93c8084e4e04607ef02af
Survi <suravitaroy@gmail.com>
2019-04-01 09:24:57 -0400

    adding components to planning page

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
 import { Grid } from "./Grid";
 import { ToolBar } from "./ToolBar";
+import { PictureList } from "./PictureList";
 //import { connect } from "react-redux";
 export class Canvas extends Component {
   state = {
-      numRows:50,
-      numCols:50,
-      canvasWidth:500
+    numRows: 50,
+    numCols: 50,
+    canvasWidth: 500,
+    pictures: [
+      { pictureId: 1, bgColor: "red" },
+      { pictureId: 2, bgColor: "blue" }
+    ]
+  };
+  componentWillMount() {
+    let grid = [];
+    for (let i = 0; i < 50; i++) {
+      let newArray = [];
+      for (let j = 0; j < 50; j++) {
+        newArray.push({ row: i, columen: j });
+      }
+      grid.push(newArray);
+    }
+    this.setState({ ...this.state, grid: grid });
   }
-  handleAddGrid = () =>{
-      this.setState({
-          numRows: this.state.numRows + 1,
-          numCols: this.state.numCols +1,
-          canvasWidth: this.state.canvasWidth + 10
-      })
-  }
-  handleSubtractGrid = ()=>{
-      this.setState({
-          numRows: this.state.numRows - 1,
-          numCols: this.state.numCols - 1,
-          canvasWidth: this.state.canvasWidth - 10
-      })
+  handleAddGrid = () => {
+    this.setState({
+      numRows: this.state.numRows + 1,
+      numCols: this.state.numCols + 1,
+      canvasWidth: this.state.canvasWidth + 10
+    });
+  };
+  handleSubtractGrid = () => {
+    this.setState({
+      numRows: this.state.numRows - 1,
+      numCols: this.state.numCols - 1,
+      canvasWidth: this.state.canvasWidth - 10
+    });
+  };
+  handleDragOver = (e) =>{
+      console.log("finish dragging");
+      e.preventDefault()
   }
 
   render() {
@@ -28,24 +48,33 @@ export class Canvas extends Component {
     for (let i = 0; i < this.state.numRows; i++) {
       let row = [];
       for (let j = 0; j < this.state.numCols; j++) {
-        row.push(<Grid key={i+","+j}/>);
+        row.push(<Grid key={i + "," + j} />);
       }
       store.push(row);
     }
 
     return (
-     <div>
-      <div><ToolBar handleAddGrid={this.handleAddGrid} handleSubtractGrid={this.handleSubtractGrid}/></div>
-      <br />
-      <div style={{ display: "flex" , width:this.state.canvasWidth+"px", flexWrap:"wrap"}}>
-        
-        {/* {store.map(row=>(
-           <Grid />
-        ))}  */}
-        {store}
-        
+      <div>
+        <div>
+          <ToolBar
+            handleAddGrid={this.handleAddGrid}
+            handleSubtractGrid={this.handleSubtractGrid}
+          />
+        </div>
+        <br />
+        <div>
+          <PictureList pictures={this.state.pictures} />
+        </div>
+        <div onDragOver={this.handleDragOver}
+          style={{
+            display: "flex",
+            width: this.state.canvasWidth + "px",
+            flexWrap: "wrap"
+          }}
+        >
+          {store}
+        </div>
       </div>
-    </div>
     );
   }
 }
