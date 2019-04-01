af583ed8dcfc967b0022bb698e669b5f97ae8565
Scott Reese <scott_reese@pm.me>
2019-04-01 16:52:49 -0400

    grid pagination added

---
+++ src/components/ToolTip.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const toolTip = props => (
+    <div
+        {...props}
+        style={{
+            backgroundColor: 'rgba(0, 0, 0, 0.85)',
+            padding: '2px 10px',
+            color: 'white',
+            borderRadius: 3,
+            ...props.style
+        }}>
+        Left click to add <br />
+        Right click to remove
+    </div>
+)
