c92f6c68d73c01feb151f9efec068086ce2723b4
Eileen <eileen.tallman@gmail.com>
2019-04-02 09:27:33 -0400

    Added routes on App.js

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+//import "./App.css";
+import {Canvas} from "./Canvas";
+import {ToolBar} from "./ToolBar"
+import { userProfilePage } from './ProfilePage'
+import { userLogin } from './Login'
+import { userRegistration } from './RegistrationPage'
+import { BrowserRouter,Switch, Route } from "react-router-dom";
+
+
+class App extends Component {
+  render() {
+    return (
+    <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <Canvas />} />
+          <Route path="/toolbar" render={() => <ToolBar />} />
+          <Route path="/profile" render={() => <userProfilePage />} />
+          <Route path="/login" render={() => <userLogin />} />
+          <Route path="/registration" render={() => <userRegistration />} />
+        </Switch>
+    
+    </BrowserRouter>
+      
+    );
+  }
+}
+
+export default App;
