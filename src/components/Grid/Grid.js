b9595a4c20bd98bec8f98f3f57ff3970522f1ae7
Scott Reese <scott_reese@pm.me>
2019-04-03 10:46:36 -0400

    deleteDroppedPlant

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
-import {cellStyle} from "./styles"
+import { cellStyle } from "./styles"
+import { OverlayTrigger } from "react-bootstrap"
+import { gridToolTip } from "./gridToolTip"
 //import { connect } from "react-redux";
 export class Grid extends Component {
     render() {
         const cellStyles = {
-            ...cellStyle, 
-            backgroundImage: this.props.image !== null ? "url("+this.props.image+")":null
+            ...cellStyle,
+            backgroundImage: this.props.image !== null ? "url(" + this.props.image + ")" : null
         }
 
         return (
-           <div data-i={this.props.i} data-j={this.props.j} draggable={this.props.image!==null ? "true" : "false"} style={cellStyles} onDragStartCapture={this.props.handleDragStart}></div>
-        ) ;  
+            <OverlayTrigger
+                placement="right-start"
+                delay={{ show: 500, hide: 250 }}
+                overlay={gridToolTip}
+            >
+                <div
+                    data-i={this.props.i}
+                    data-j={this.props.j}
+                    draggable={this.props.image !== null ? "true" : "false"}
+                    style={cellStyles}
+                    onDragStartCapture={this.props.handleDragStart}
+                    onContextMenu={this.props.contextMenu}
+                />
+            </OverlayTrigger>
+        );
     }
 }
 
