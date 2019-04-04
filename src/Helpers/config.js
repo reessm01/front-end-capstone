2bd310f09418d6faa25c2898ca7655fe133a43c3
Missi Perez <perezmissi@aol.com>
2019-04-04 09:40:38 -0400

    added actions for spreadsheet

--- src/Helpers/config.js
+++ src/Helpers/config.js
@@ -1,8 +1,7 @@
-// export default {
-//     apiKey: "AIzaSyDjrGpJCMGvvHt087ETU4K3jDLx2edJIok"
-//     discoveryDocs: 
-//     spreadsheetId: "1cwgx2mijD_OlmJYeTj-d-7U6VUqa8ot3cXexOgaJVRY" - this is correct
-//   };
+export default {
+    apiKey: "AIzaSyDjrGpJCMGvvHt087ETU4K3jDLx2edJIok",
+    discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    spreadsheetId: "1cwgx2mijD_OlmJYeTj-d-7U6VUqa8ot3cXexOgaJVRY" 
+  };
 
 
-// this is where the google doc needs to be linked 
